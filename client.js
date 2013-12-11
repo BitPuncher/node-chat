10356dad9931f2e4a87c6dde744b0914491cdbd6
Jefferson Leard <jtleard@gmail.com>
2013-12-10 23:04:33 -0800

    Initial commit. Working copy.

---
+++ client.js
@@ -0,0 +1,34 @@
+repl = require('repl');
+read = require('read');
+util = require('util');
+
+var server_prompt = function(data) {
+	console.log(data['prompt']);
+	read(
+	  {},
+		function (err, input, isDefault) {
+			socket.emit('input', { 'input': '/login ' + input });
+		});
+};
+
+
+socket = require('socket.io-client').connect('http://localhost:8080');
+socket.on('connect', function(){
+  socket.on('output', function(data){
+  	console.log(data['output']);
+  });
+
+  socket.on('login', server_prompt)
+
+  socket.on('logged_in', function(){
+  	repl.start({
+			'prompt':'==> ',
+			'eval': function(cmd, context, filename, callback){
+				socket.emit('input', { input: cmd.substring(1, cmd.length - 2)} );
+			},
+	  });
+  });
+
+  socket.on('disconnect', function(){});
+
+});
---
+++ server.js
@@ -0,0 +1,127 @@
+var app = require('http').createServer(handler)
+  , io = require('socket.io').listen(app)
+  , fs = require('fs'), _ = require('underscore')
+
+app.listen(8080);
+
+var commands = {
+  '/join': function(socket, input) {
+    socket.leave(socket.currentRoom);
+    socket.broadcast.to(socket.currentRoom).emit('output',
+      { output:"<== " + socket.username + " has left the channel."});
+
+    socket.join(input);
+    socket.currentRoom = input;
+
+    socket.broadcast.to(socket.currentRoom).emit('output', { output:"<== " + 
+      socket.username + " joins the channel.\n" });
+
+    var allUsers = []
+    io.sockets.clients(socket.currentRoom).forEach(function (el) {
+      allUsers.push(el.username);
+    });
+
+    var outputString = "<== entering room " + socket.currentRoom + ".\n"
+
+    allUsers.forEach(function (el) {
+      if (el == socket.username) {
+        outputString += "<== * " + el + " (** this is you)\n"
+      } else {
+        outputString += "<== * " + el + "\n";
+      }
+    });
+
+    outputString += "<== end of list.\n";
+
+    socket.emit('output', { 'output':outputString });
+  },
+
+  '/leave': function(socket) {
+    if (socket.currentRoom != "lobby") {
+      this['/join'](socket, 'lobby');
+    } else {
+      socket.emit('output', { output: "<== You are in the lobby.\n"})
+    }
+  },
+
+  '/send': function(socket, input) {
+    io.sockets.in(socket.currentRoom).emit('output', { 
+          'output':"<== " + socket.username + ": " + input });
+  },
+
+  '/rooms': function(socket) {
+    outputString = "<== List of channels. /join to create.\n";
+    Object.keys(io.sockets.manager.rooms).forEach(function(room) {
+      outputString += "<== * " + room + "\n";
+    })
+    outputString += "<== end of list.";
+
+    socket.emit('output', { output: outputString });
+  },
+
+  '/help': function(socket) {
+    outputString = "<== List of commands.\n";
+    Object.keys(this).forEach(function(room) {
+      outputString += "<== * " + room + "\n";
+    })
+    outputString += "<== end of list.";
+
+    socket.emit('output', { output: outputString });
+  },
+
+  '/login': function(socket, input) {
+    if (_.include(used_names, input)){
+      socket.emit('login', {prompt:"<== Name taken.\n<== Login Name?"})
+    }
+    else {
+      used_names.push(input);
+      socket.username = input;
+      socket.emit('logged_in');
+      var outputString = "<== Welcome, " + socket.username + ".\n";
+      socket.emit('output', { output: outputString })
+      this['/join'](socket, 'lobby');
+    }
+  }
+};
+
+var rooms = {
+  lobby:0,
+};
+
+var users = {
+
+};
+
+var used_names = [];
+
+
+
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+var event_router = function (input) {
+  if (input['input'][0] == '/') {
+    var command = input['input'].match(/^\S+/)[0];
+    input = input['input'].substring(command.length + 1, input.length);
+
+    commands[command](this, input);
+  } else {
+    commands['/send'](this, input['input']);
+  }
+}
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('login', { prompt:"<== Welcome to XYZ chat server.\n" + 
+    "<== Login Name?" });
+  socket.on('input', event_router);
+});
